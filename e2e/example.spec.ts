4092f025313201fba7c549eabc0cb45f2dd5bc0a
STimour <st.sayfoutdinov@icloud.com>
2023-11-29 18:17:37 +0100

    correction

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,15 +2,19 @@ import { test, expect } from '@playwright/test';
 
 
 test("Count = 0", async ({ page }) => {
+  
   await page.goto('http://localhost:5173/')
   
+
   const countValue = await page.innerText('button')
 
   expect(countValue).toContain('0')
 })
 
 test("click work", async ({ page }) => {
+  
   await page.goto('http://localhost:5173/')
+
 
   await page.click('.card button')
   await page.click('.card button')
